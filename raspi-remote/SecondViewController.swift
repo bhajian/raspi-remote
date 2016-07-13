19a4a77b97d48a8daa30d8f1ffddf4b54fd9d060
Behnam Hajian <behnam@ca.ibm.com>
2016-07-13 15:20:26 -0400

    init commit

---
+++ raspi-remote/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  raspi-remote
+//
+//  Created by behnam hajian on 2016-07-13.
+//  Copyright © 2016 behnam hajian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
