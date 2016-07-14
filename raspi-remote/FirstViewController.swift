63496375903096a118ceded953928118ce8383ee
Behnam Hajian <behnam@ca.ibm.com>
2016-07-14 17:17:23 -0400

    adding functionality to command/.

--- raspi-remote/FirstViewController.swift
+++ raspi-remote/FirstViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import TextToSpeechV1
 import AVFoundation
 import SpeechToTextV1
+import Foundation
 
 class FirstViewController: UIViewController {
 
@@ -17,6 +18,8 @@ class FirstViewController: UIViewController {
     var captureSession: AVCaptureSession?
     var recorder: AVAudioRecorder!
     @IBOutlet weak var transcribedLabel: UILabel!
+    var session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+    var baseServiceUrl = "http://behnam.mybluemix.net/"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,13 +70,103 @@ class FirstViewController: UIViewController {
                 let settings = TranscriptionSettings(contentType: .WAV)
                 let failure = { (error: NSError) in print(error) }
                 speechToText.transcribe(recorder.url, settings: settings, failure: failure){
-                    result in if let Transcription = result.last?.alternatives.last?.transcript{
-                        self.transcribedLabel.text = Transcription
+                    result in if let transcription = result.last?.alternatives.last?.transcript{
+                        self.transcribedLabel.text = transcription
+                        
+                        let command = transcription.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()).lowercaseString
+                        
+                        if(command == "forward"){
+                            self.forwardTouchInside(sender)
+                        }
+                        if(command == "backward"){
+                            self.backwardTouchInside(sender)
+                        }
+                        if(command == "turn left"){
+                            self.leftTouchInside(sender)
+                        }
+                        if(command == "turn right"){
+                            self.rightTouchInside(sender)
+                        }
+                        if(command == "stop"){
+                            self.stopTouchInside(sender)
+                        }
                     }
                 }
             } catch {}
         }
     }
+    
+    private func makeGetRequest(request: NSURLRequest){
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+            if let data = data {
+                let response = NSString(data: data, encoding: NSUTF8StringEncoding)
+                print(response)
+            }
+        }
+        task.resume()
+    }
+    
+    
+    @IBAction func forwardTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "car/goForward")!)
+        makeGetRequest(request);
+    }
+    
+
+    @IBAction func backwardTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "car/goBackward")!)
+        makeGetRequest(request);
+    }
+    @IBAction func rightTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "car/turnRight/coarse")!)
+        makeGetRequest(request);
+    }
+    
+    @IBAction func leftTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "car/turnLeft/coarse")!)
+        makeGetRequest(request);
+    }
+    
+    @IBAction func stopTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "car/stop")!)
+        makeGetRequest(request);
+    }
+    
+    @IBAction func speedValueChanged(sender: UISlider) {
+        let selectedValue = Int(sender.value)
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "car/setSpeed/" + String(selectedValue))!)
+        makeGetRequest(request);
+    }
+    
+    @IBAction func cameraUpTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "camera/up")!)
+        makeGetRequest(request);
+    }
+    
+    @IBAction func cameraDownTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "camera/down")!)
+        makeGetRequest(request);
+    }
+    
+    @IBAction func cameraRightTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "camera/right")!)
+        makeGetRequest(request);
+    }
+    
+    @IBAction func cameraLeftTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "camera/left")!)
+        makeGetRequest(request);
+    }
+    
+    @IBAction func cameraHomeTouchInside(sender: AnyObject) {
+        let request = NSURLRequest(URL: NSURL(string: baseServiceUrl + "camera/home")!)
+        makeGetRequest(request);
+    }
+    
+    
+    
+    
+    
 
 }
 
